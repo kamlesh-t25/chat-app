2a3ce2bfd00ff658643fc60598c062fd8c078bd1
Kamlesh Takhar <220010025@iitdh.ac.in>
2024-08-08 20:38:53 +0530

    t

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,17 @@
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
+
 dotenv.config();
-export const connectDB=async()=>{
+
+export const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGOOSE_URL).then(()=>{
-            console.log("DB connected");
-        })
+        await mongoose.connect(process.env.MONGOOSE_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 30000, // Increase timeout to 30 seconds
+        });
+        console.log("DB connected");
     } catch (error) {
-       console.log(error); 
+        console.log("Error connecting to DB:", error);
     }
 }
