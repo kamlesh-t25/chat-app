55703912718056c1a709db0a31ab5b193487e02e
Kamlesh Takhar <220010025@iitdh.ac.in>
2024-08-08 20:16:01 +0530

    t

--- server/socket/index.js
+++ server/socket/index.js
@@ -10,12 +10,12 @@ const app=express();
 
 // socket connection 
 const server=http.createServer(app);
-const io=new Server(server,{
-    cors:{
-        origin:process.env.FRONTEND_URL,
-        credentials:true
-    }
-})
+app.use(cors({
+    origin: '*', // Allow all origins
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+    credentials: true // Allow credentials (cookies, authorization headers, etc.)
+  }));
 
 // * socket running at http://localhost:8080/*
 const onlineUser=new Set();
